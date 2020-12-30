38a095349673ee92bf8289d30d1d025b2f9caf1b
dev-93 <cr000@naver.com>
2020-12-31 00:24:18 +0900

    동적으로 Head 삽입

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,44 +1,36 @@
 import axios from 'axios';
+import Head from 'next/head';
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react';
 import { Loader } from 'semantic-ui-react';
 import Item from '../../src/component/item';
 
-const Id = () => {
-    const router = useRouter();
-    const { id } = router.query;
-    const [item, setItem] = useState({});
-
-    const [isLoading, setIsLoading] = useState(true);
-    
-    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-
-    function getData() {
-        axios.get(API_URL)
-        .then(res => {
-            console.log(res.data);
-            setItem(res.data);
-            setIsLoading(false);
-        })
-    };
-
-    useEffect(()=>{
-        if(id && id > 0) {
-            getData();
-        }
-    }, [id])
-
+const Post = ({item}) => {
     return (
         <>
-        {
-            isLoading ?
-                <Loader inline="centerd" active>
-                    Loading
-                </Loader> :
-                <Item item={item}/>
-        }
+            {item && (
+                <>
+                    <Head>
+                        <title>{item.name}</title>
+                        <meta name="description" content={item.description}></meta>
+                    </Head>
+                    <Item item={item}/>
+                </>
+            )}
         </>   
     )
 }
+export default Post;
 
-export default Id
+export async function getServerSideProps(context) {
+    const id = context.params.id;
+    const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+    const res = await axios.get(apiUrl);
+    const data = res.data;
+
+    return {
+        props: {
+            item: data,
+        }
+    }
+}

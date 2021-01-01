aa4db68732a4d313b5eb9c322a460b85117af2d4
dev-93 <cr000@naver.com>
2021-01-01 21:55:42 +0900

    수정 중

--- pages/index.js
+++ pages/index.js
@@ -4,48 +4,36 @@ import { useEffect, useState } from 'react';
 import { Divider, Header, Loader } from 'semantic-ui-react';
 import ItemList from '../src/component/ItemList';
 
-export default function Home() {
-    const [list, setList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-     
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    
-    function getData() {
-        axios.get(API_URL)
-        .then(res => {
-            setList(res.data);
-            setIsLoading(false);
-        })
-    }
-
-    useEffect(() => {
-        getData();
-    }, []);
-
+export default function Home({list}) {
     return (
         <>
             <Head>
                 <title>Home | 태남</title>
             </Head>
-            {
-                isLoading ? 
-                <>
-                    <Loader active>
-                        Loading
-                    </Loader>
-                </> : 
-                <>
-                    <Header as="h3" style={{paddingTop: 40}}>
-                        Best
-                    </Header>
-                    <Divider />
-                    <ItemList list={list.slice(0,9)}/ >
-                    <Header as="h3" style={{paddingTop: 40}}>
-                        New
-                    </Header>
-                    <ItemList list={list.slice(9)}/ >
-                </>
-            }
+            <>
+                <Header as="h3" style={{paddingTop: 40}}>
+                    Best
+                </Header>
+                <Divider />
+                <ItemList list={list.slice(0,9)}/ >
+                <Header as="h3" style={{paddingTop: 40}}>
+                    New
+                </Header>
+                <ItemList list={list.slice(9)}/ >
+            </>
         </>
     )
+};
+
+export async function getStaticProps() {
+    const api_url = process.env.apiUrl;
+    const res = await axios.get(apiUrl);
+    const data = res.data;
+
+    return {
+        props: {
+            list: data,
+            name: process.env.name
+        }
+    }
 }

04c70d58e7afdd03ef8653b467ed20cb940c5906
dev-93 <cr000@naver.com>
2021-01-02 00:20:23 +0900

    정적 생성 표현

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,34 @@
 import axios from 'axios';
 import Head from 'next/head'
-import { useEffect, useState } from 'react';
 import { Divider, Header, Loader } from 'semantic-ui-react';
 import ItemList from '../src/component/ItemList';
 
-export default function Home({list}) {
-    return (
-        <>
+export default function Home ({list}) {
+    return(
+        <div>
             <Head>
-                <title>Home | 태남</title>
-            </Head>
-            <>
-                <Header as="h3" style={{paddingTop: 40}}>
-                    Best
-                </Header>
-                <Divider />
-                <ItemList list={list.slice(0,9)}/ >
-                <Header as="h3" style={{paddingTop: 40}}>
-                    New
-                </Header>
-                <ItemList list={list.slice(9)}/ >
-            </>
-        </>
-    )
-};
+                 <title>Home | 태남</title>
+             </Head>
+             <>
+                 <Header as="h3" style={{paddingTop: 40}}>
+                     Best
+                 </Header>
+                 <Divider />
+                 <ItemList list={list.slice(0,9)}/ >
+                 <Header as="h3" style={{paddingTop: 40}}>
+                     New
+                 </Header>
+                 <Divider />
+                 <ItemList list={list.slice(9)}/ >
+             </>
+        </div>
+    )    
+}
+
 
 export async function getStaticProps() {
     const api_url = process.env.apiUrl;
-    const res = await axios.get(apiUrl);
+    const res = await axios.get(api_url);
     const data = res.data;
 
     return {

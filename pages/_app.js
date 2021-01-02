de56d83d0368292e459af73684dc98f412a747f2
dev-93 <cr000@naver.com>
2021-01-02 12:49:20 +0900

    viewport meta tags warning

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css';
 import 'semantic-ui-css/semantic.min.css';
 import Footer from '../src/component/Footer';
 import Top from '../src/component/Top';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
       <Top/>
       <Component {...pageProps} />
       <Footer/>
